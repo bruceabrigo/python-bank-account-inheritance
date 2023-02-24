18a499658a06389a628bddb74f2904dc6e59242e
bruceabrigo <bruceabrigo@outlook.com>
2023-02-24 14:04:49 -0600

    Users can now deposit to basic account

--- bank.py
+++ bank.py
@@ -1,5 +1,13 @@
 class BankAccount:
-  pass
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def __str__(self):
+       return f'{self.balance}'
+    
+    # define a deposit method
+    def deposit(self, deposit):
+       self.balance = deposit
 
 class ChildrensAccount:
   pass
@@ -9,26 +17,25 @@ class OverdraftAccount:
 
 basic_account = BankAccount()
 basic_account.deposit(600)
+# basic_account.withdraw(17)
 print("Basic account has ${}".format(basic_account.balance))
-basic_account.withdraw(17)
-print("Basic account has ${}".format(basic_account.balance))
-basic_account.accumulate_interest()
-print("Basic account has ${}".format(basic_account.balance))
-print()
+# basic_account.accumulate_interest()
+# print("Basic account has ${}".format(basic_account.balance))
+# print()
 
-childs_account = ChildrensAccount()
-childs_account.deposit(34)
-print("Child's account has ${}".format(childs_account.balance))
-childs_account.withdraw(17)
-print("Child's account has ${}".format(childs_account.balance))
-childs_account.accumulate_interest()
-print("Child's account has ${}".format(childs_account.balance))
-print()
+# childs_account = ChildrensAccount()
+# childs_account.deposit(34)
+# print("Child's account has ${}".format(childs_account.balance))
+# childs_account.withdraw(17)
+# print("Child's account has ${}".format(childs_account.balance))
+# childs_account.accumulate_interest()
+# print("Child's account has ${}".format(childs_account.balance))
+# print()
 
-overdraft_account = OverdraftAccount()
-overdraft_account.deposit(12)
-print("Overdraft account has ${}".format(overdraft_account.balance))
-overdraft_account.withdraw(17)
-print("Overdraft account has ${}".format(overdraft_account.balance))
-overdraft_account.accumulate_interest()
-print("Overdraft account has ${}".format(overdraft_account.balance))
+# overdraft_account = OverdraftAccount()
+# overdraft_account.deposit(12)
+# print("Overdraft account has ${}".format(overdraft_account.balance))
+# overdraft_account.withdraw(17)
+# print("Overdraft account has ${}".format(overdraft_account.balance))
+# overdraft_account.accumulate_interest()
+# print("Overdraft account has ${}".format(overdraft_account.balance))
